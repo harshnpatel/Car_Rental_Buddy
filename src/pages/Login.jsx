c4ff07fc96d0cdd2955dcb13c3a392140f1d1b71
harshnpatel <111911582+harshnpatel@users.noreply.github.com>
2024-04-08 11:49:36 +0530

    First Commit
    Date:- 7/24

---
+++ src/pages/Login.jsx
@@ -0,0 +1,147 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div>
+      <>
+  <meta charSet="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Document</title>
+  <link
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+    rel="stylesheet"
+  />
+  {/* Google Fonts */}
+  <link
+    href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+    rel="stylesheet"
+  />
+  <link
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+    rel="stylesheet"
+  />
+  {/* Google Fonts */}
+  <link
+    href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+    rel="stylesheet"
+  />
+  {/* MDB */}
+  <link
+    href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.2.0/mdb.min.css"
+    rel="stylesheet"
+  />
+  <style
+    dangerouslySetInnerHTML={{
+      __html:
+        '\n  .divider:after,\n.divider:before {\ncontent: "";\nflex: 1;\nheight: 1px;\nbackground: #eee;\n}\n.h-custom {\nheight: calc(100% - 73px);\n}\n@media (max-width: 450px) {\n.h-custom {\nheight: 100%;\n}\n}\n'
+    }}
+  />
+  <section className="vh-90" style={{ backgroundColor: "#9A616D" }}>
+    <div className="container py-5 h-100">
+      <div className="row d-flex justify-content-center align-items-center h-100">
+        <div className="col col-xl-10">
+          <div className="card" style={{ borderRadius: "1rem" }}>
+            <div className="row g-0">
+              <div className="col-md-6 col-lg-5 d-none d-md-block">
+                <img
+                  src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/img1.webp"
+                  alt="login form"
+                  className="img-fluid"
+                  style={{ borderRadius: "1rem 0 0 1rem" }}
+                />
+              </div>
+              <div className="col-md-6 col-lg-7 d-flex align-items-center">
+                <div className="card-body p-4 p-lg-5 text-black">
+                  <form>
+                    <div className="d-flex align-items-center mb-3 pb-1">
+                      <i
+                        className="fas fa-cubes fa-2x me-3"
+                        style={{ color: "#ff6219" }}
+                      />
+                      <span className="h1 fw-bold mb-0">Logo</span>
+                    </div>
+                    {/* <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Sign into your account</h5> */}
+                    <div className="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
+                      <p className="lead fw-normal mb-0 me-3">Sign in with</p>
+                      <button
+                        type="button"
+                        className="btn btn-primary btn-floating mx-1"
+                      >
+                        <i className="fab fa-facebook-f" />
+                      </button>
+                      <button
+                        type="button"
+                        className="btn btn-primary btn-floating mx-1"
+                      >
+                        <i className="fab fa-twitter" />
+                      </button>
+                      <button
+                        type="button"
+                        className="btn btn-primary btn-floating mx-1"
+                      >
+                        <i className="fab fa-linkedin-in" />
+                      </button>
+                    </div>
+                    <div className="divider d-flex align-items-center my-4">
+                      <p className="text-center fw-bold mx-3 mb-0">Or</p>
+                    </div>
+                    <div className="form-outline mb-4">
+                      <input
+                        type="email"
+                        id="form2Example17"
+                        className="form-control form-control-lg"
+                      />
+                      <label className="form-label" htmlFor="form2Example17">
+                        Email address
+                      </label>
+                    </div>
+                    <div className="form-outline mb-4">
+                      <input
+                        type="password"
+                        id="form2Example27"
+                        className="form-control form-control-lg"
+                      />
+                      <label className="form-label" htmlFor="form2Example27">
+                        Password
+                      </label>
+                    </div>
+                    <div className="pt-1 mb-4">
+                      <a href='Home'>
+                      <button
+                        className="btn btn-dark btn-lg btn-block"
+                        type="button">
+                        Login
+                      </button>
+                      </a>
+                    </div>
+                    <a className="small text-muted" href="#!">
+                      Forgot password?
+                    </a>
+                    <p className="mb-5 pb-lg-2" style={{ color: "#393f81" }}>
+                      Don't have an account?{" "}
+                      <a href="#!" style={{ color: "#393f81" }}>
+                        Register here
+                      </a>
+                    </p>
+                    <a href="#!" className="small text-muted">
+                      Terms of use.
+                    </a>
+                    <a href="#!" className="small text-muted">
+                      Privacy policy
+                    </a>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</>
+
+    </div>
+  )
+}
+
+export default Login
